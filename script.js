f6573d8142176507149084a9138ca2ab8e099fa1
om-bhagat <139191838+om-bhagat@users.noreply.github.com>
2026-08-02 15:42:22 -0400

    Add basic Notes application window

--- script.js
+++ script.js
@@ -28,3 +28,32 @@ closeWindowButtons.forEach(button => {
         }
     });
 });
+
+const saveNoteButton =
+    document.getElementById("save-note-button");
+
+const clearNoteButton =
+    document.getElementById("clear-note-button");
+
+const noteTitle =
+    document.getElementById("note-title");
+
+const noteText =
+    document.getElementById("note-text");
+
+const notesMessage =
+    document.getElementById("notes-message");
+
+if (saveNoteButton) {
+    saveNoteButton.addEventListener("click", () => {
+        notesMessage.text = "Note saved.";
+    });
+}
+
+if (clearNoteButton) {
+    clearNoteButton.addEventListener("click", () => {
+        noteTitle.value = "";
+        noteText.value = "";
+        notesMessage.textContent = "Note cleared.";
+    });
+}
